edd6eb231a44da7ddd525a0a4db8999a2f344363
dehasi <rgaleyev@gmail.com>
2018-07-06 22:37:23 +0200

    ODSC04-2: Test groupedPostings

--- stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -1,26 +1,39 @@
 package stackoverflow
 
-import org.scalatest.{FunSuite, BeforeAndAfterAll}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.rdd.RDD
-import java.io.File
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 @RunWith(classOf[JUnitRunner])
 class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
 
+  val sc = StackOverflow.sc
+  private val question = 1
+  private val answer__ = 2
+
+  /* <postTypeId>, <id>, <acceptedAnswer>, <parentId>, <score>, <tag> */
+  val posts = List(
+    Posting(question, 1, Some(2), None, 1, Option("Java")),
+    Posting(answer__, 2, None, Some(1), 1, Option("Java")),
+    Posting(question, 3, Some(4), None, 2, Option("Scala")),
+    Posting(answer__, 4, None, Some(3), 3, Option("Scala")),
+    Posting(question, 5, None, None, 2, Option("Scala"))
+  )
+
+  val postsRDD = sc.parallelize(posts)
 
   lazy val testObject = new StackOverflow {
     override val langs =
       List(
         "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
         "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+
     override def langSpread = 50000
+
     override def kmeansKernels = 45
+
     override def kmeansEta: Double = 20.0D
+
     override def kmeansMaxIterations = 120
   }
 
@@ -34,5 +47,8 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
     assert(instantiatable, "Can't instantiate a StackOverflow object")
   }
 
-
+  test("groupedPostings") {
+    val grouped = StackOverflow.groupedPostings(postsRDD).collect()
+    assert(grouped.length == 2)
+  }
 }
